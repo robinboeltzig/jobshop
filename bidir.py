04ffd46560b601b93876c03843081796ffe16c67
lukaszimmermann1997 <lzlukaszimmermann@gmail.com>
2020-02-07 09:44:18 +0100

    bidir

--- bidir.py
+++ bidir.py
@@ -9,30 +9,35 @@ def doBidir(Graph):
     r = {}
     q = {}
     for n in Graph.edges:
-        if n[2] == 2:
+        if n[2] == 0:
             if n[0] == 0:
-                s.add(n[1])
-            if n[0] == 1:
-                t.add(n[1])
-            if n[1] == 0:
-                s.add(n[0])
+               s.add(n[1])
             if n[1] == 1:
                 t.add(n[0])
+        if n[2] == 1:
+            if n[1] == 0:
+                s.add(n[0])
+            if n[0] == 1:
+                t.add(n[1])
+    print()
     for n in s:
         r[n] = 0
     for n in t:
         q[n] = 0
 
     while len(left.union(right)) != len(Graph.nodes):
-        i = random.sample(s,1)
+        i = random.sample(s,1)[0]
+        print(i)
         k = set()
         for n in Graph.nodes:
-            if not n[4] in left:
-                k.add(n[4])
+            #print (Graph.nodes)
+            #print(n)
+            if not Graph.nodes[n][4] in left:
+                k.add(Graph.nodes[n][4])
         for n in Graph.edges:
-            if n[0] == i and not n[1] in k:
+            if n[0] == i and n[1] in k and n[2] == 2:
                 n[2] = 0
-            if not n[0] in k and n[1] == i:
+            if n[0] in k and n[1] == i and n[2] == 2:
                 n[2] = 1
         s.remove(i)
         left.add(i)
@@ -40,64 +45,68 @@ def doBidir(Graph):
         for n in Graph.edges:
             if n[0] == i and n[2] == 0:
                 for m in Graph.nodes:
-                    if m[2] == Graph.nodes[i][2] and m[3] == Graph.nodes[i][3]+1:
-                        succ = m[3]
+                    if Graph.nodes[m][2] == Graph.nodes[i][2] and Graph.nodes[m][3] == Graph.nodes[i][3]+1:
+                        succ = Graph.nodes[m][4]
+                        if succ not in right:
+                            s.add(succ)
             elif n[1] == i and n[2] == 1:
                 for m in Graph.nodes:
-                    if m[2] == Graph.nodes[i][2] and m[3] == Graph.nodes[i][3]+1:
-                        succ = m[3]
-        if not succ in right:
-            s.add(succ)
+                    if Graph.nodes[m][2] == Graph.nodes[i][2] and Graph.nodes[m][3] == Graph.nodes[i][3]+1:
+                        succ = Graph.nodes[m][4]
+                        if succ not in right:
+                            s.add(succ)
         for n in r:
             x = set()
             for m in Graph.nodes:
-                if m[4] in r:
-                    if m[3] == 0:
+                if Graph.nodes[m][4] in r:
+                    if Graph.nodes[m][3] == 0:
                         r[n] = 0
                     else:
                         for b in Graph.nodes:
-                            if b[2] == m[2] and b[3] < m[3]:
-                                x.add(b[4])
+                            if Graph.nodes[b][2] == Graph.nodes[m][2] and Graph.nodes[b][3] < Graph.nodes[m][3]:
+                                x.add(Graph.nodes[b][4])
             dist = 0
             for m in x:
                 dist = dist + m
             r[n] = dist
-    if len(left.union(right)) != len(Graph.nodes):
-        i = random.sample(t,1)
-        j = set()
-        for n in Graph.nodes:
-            if not n[4] in right:
-                j.add(n[4])
-        for n in Graph.edges:
-            if n[0] == i and not n[1] in j:
-                n[2] = 0
-            if not n[0] in j and n[1] == i:
-                n[2] = 1
-        t.remove(i)
-        right.add(i)
-        s.discard(i)
-        for n in Graph.edges:
-            if n[0] == i and n[2] == 1:
+        if len(left.union(right)) != len(Graph.nodes):
+            i = random.sample(t,1)[0]
+            j = set()
+            for n in Graph.nodes:
+                if not Graph.nodes[n][4] in right:
+                    j.add(Graph.nodes[n][4])
+            for n in Graph.edges:
+                if n[0] == i and n[1] in j and n[2] == 2:
+                    n[2] = 1
+                if n[0] in j and n[1] == i and n[2] == 2:
+                    n[2] = 0
+            t.remove(i)
+            right.add(i)
+            s.discard(i)
+            for n in Graph.edges:
+                if n[0] == i and n[2] == 1:
+                    for m in Graph.nodes:
+                        if Graph.nodes[m][2] == Graph.nodes[i][2] and Graph.nodes[m][3] == Graph.nodes[i][3]-1:
+                            pred = Graph.nodes[m][4]
+                            if pred not in left:
+                                t.add(pred)
+                elif n[1] == i and n[2] == 0:
+                    for m in Graph.nodes:
+                        if Graph.nodes[m][2] == Graph.nodes[i][2] and Graph.nodes[m][3] == Graph.nodes[i][3]-1:
+                            pred = Graph.nodes[m][4]
+                            if pred not in left:
+                                t.add(pred)
+            for n in q:
+                x = set()
                 for m in Graph.nodes:
-                    if m[2] == Graph.nodes[i][2] and m[3] == Graph.nodes[i][3]-1:
-                        pred = m[3]
-            elif n[1] == i and n[2] == 1:
-                for m in Graph.nodes:
-                    if m[2] == Graph.nodes[i][2] and m[3] == Graph.nodes[i][3]-1:
-                        pred = m[3]
-        if not pred in left:
-            t.add(pred)
-        for n in q:
-            x = set()
-            for m in Graph.nodes:
-                if m[4] in q:
-                    if m[3] == 0:
-                        q[n] = 0
-                    else:
-                        for b in Graph.nodes:
-                            if b[2] == m[2] and b[3] > m[3]:
-                                x.add(b[4])
-            dist = 0
-            for m in x:
-                dist = dist + m
-            q[n] = dist
+                    if Graph.nodes[m][4] in q:
+                        if Graph.nodes[m][3] == 0:
+                            q[n] = 0
+                        else:
+                            for b in Graph.nodes:
+                                if Graph.nodes[b][2] == Graph.nodes[m][2] and Graph.nodes[b][3] > Graph.nodes[m][3]:
+                                    x.add(Graph.nodes[b][4])
+                dist = 0
+                for m in x:
+                    dist = dist + m
+                q[n] = dist
